e9d0ef3dfa141e5f8c0eda824e3e098125e3c7a8
zalobmq <gbretonesmoraquero@gmail.com>
2022-03-03 16:46:31 +0100

    Clase paquete envio

---
+++ src/main/java/modeloPaqueteEnvio/PaqueteEnv.java
@@ -0,0 +1,75 @@
+package modeloPaqueteEnvio;
+
+import java.io.Serializable;
+
+public class PaqueteEnv implements Serializable{
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected int opcion;
+    protected Object objeto1;
+    protected Object objeto2;
+    protected boolean comprobante;
+    
+    
+    public PaqueteEnv() {
+        super();
+    }
+
+    public PaqueteEnv(int opcion, Object objeto1, boolean comprobante) {
+        super();
+        this.opcion = opcion;
+        this.objeto1 = objeto1;
+        this.comprobante = comprobante;
+    }
+
+    public PaqueteEnv(int opcion, Object objeto1, Object objeto2, boolean comprobante) {
+        super();
+        this.opcion = opcion;
+        this.objeto1 = objeto1;
+        this.objeto2 = objeto2;
+        this.comprobante = comprobante;
+    }
+
+    public int getOpcion() {
+        return opcion;
+    }
+
+    public void setOpcion(int opcion) {
+        this.opcion = opcion;
+    }
+
+    public Object getObjeto1() {
+        return objeto1;
+    }
+
+    public void setObjeto1(Object objeto1) {
+        this.objeto1 = objeto1;
+    }
+
+    public Object getObjeto2() {
+        return objeto2;
+    }
+
+    public void setObjeto2(Object objeto2) {
+        this.objeto2 = objeto2;
+    }
+
+    public boolean isComprobante() {
+        return comprobante;
+    }
+
+    public void setComprobante(boolean comprobante) {
+        this.comprobante = comprobante;
+    }
+
+    @Override
+    public String toString() {
+        return "PaqueteEnv [opcion=" + opcion + ", objeto1=" + objeto1 + ", objeto2=" + objeto2 + ", comprobante="
+                + comprobante + "]";
+    }
+    
+    
+}
